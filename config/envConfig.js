c032496774bc8761a8467ec9d21b0a5601e5794e
Tristan Teo <0xpapismash@gmail.com>
2023-02-22 18:02:14 +0800

    set up envConfig

---
+++ config/envConfig.js
@@ -0,0 +1,46 @@
+// eslint-disable-next-line import/extensions
+import dotenv from 'dotenv';
+import { mainnetAddresses, testnetAddresses } from '../globals/flowAddresses.js';
+
+dotenv.config();
+
+const env = {
+  development: {
+    contractFungibleToken: testnetAddresses.fungibleToken,
+    contractNonFungibleToken: testnetAddresses.nonFungibleToken,
+    contractMetadataViews: testnetAddresses.metadataViews,
+    contractFlowToken: testnetAddresses.flowToken,
+    contractNftStorefrontV2: testnetAddresses.nftStorefrontV2,
+    adminAddress: process.env.ADMIN_TESTNET_ADDRESS,
+    adminPrivateKeyIndex0: process.env.ADMIN_TESTNET_PRIVATE_KEY,
+    adminPrivateKeyIndex1: process.env.ADMIN_TESTNET_PRIVATE_KEY_INDEX_1,
+    adminPrivateKeyIndex2: process.env.ADMIN_TESTNET_PRIVATE_KEY_INDEX_2,
+    flowAccessNodeApi: 'https://rest-testnet.onflow.org',
+    flowDiscoveryWallet: 'https://fcl-discovery.onflow.org/testnet/authn',
+    flowNetwork: 'testnet',
+  },
+  production: {
+    contractFungibleToken: mainnetAddresses.fungibleToken,
+    contractNonFungibleToken: mainnetAddresses.nonFungibleToken,
+    contractMetadataViews: mainnetAddresses.metadataViews,
+    contractFlowToken: mainnetAddresses.flowToken,
+    contractNftStorefrontV2: mainnetAddresses.nftStorefrontV2,
+    adminAddress: process.env.ADMIN_MAINNET_ADDRESS,
+    adminPrivateKeyIndex0: process.env.ADMIN_MAINNET_PRIVATE_KEY,
+    adminPrivateKeyIndex1: process.env.ADMIN_MAINNET_PRIVATE_KEY_INDEX_1,
+    adminPrivateKeyIndex2: process.env.ADMIN_MAINNET_PRIVATE_KEY_INDEX_2,
+    flowAccessNodeApi: 'https://rest-mainnet.onflow.org',
+    flowDiscoveryWallet: 'https://fcl-discovery.onflow.org/authn',
+    flowNetwork: 'mainnet',
+  },
+};
+
+// check process.env.NODE_ENV if its `production`, if not, its `development`
+const currentEnv = () => {
+  if (process.env.NODE_ENV === 'production') {
+    return 'production';
+  }
+  return 'development';
+};
+
+export default env[currentEnv()];
---
+++ globals/flowAddresses.js
@@ -0,0 +1,15 @@
+export const testnetAddresses = {
+  fungibleToken: '0x9a0766d93b6608b7',
+  nonFungibleToken: '0x631e88ae7f1d7c20',
+  metadataViews: '0x631e88ae7f1d7c20',
+  flowToken: '0x7e60df042a9c0868',
+  nftStorefrontV2: '0x2d55b98eb200daef',
+};
+
+export const mainnetAddresses = {
+  fungibleToken: '0xf233dcee88fe0abe',
+  nonFungibleToken: '0x1d7e57aa55817448',
+  metadataViews: '0x1d7e57aa55817448',
+  flowToken: '0x1654653399040a61',
+  nftStorefrontV2: '0x4eb8a10cb9f87357',
+};
